7127380c22203d8d6057d8bb4d007d0cf7ad0591
Shivam Pandey <94222502+shivampandey811@users.noreply.github.com>
2024-04-11 21:18:59 +0530

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,7 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     deadline = models.DateTimeField(null=True, blank=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    voters = models.ManyToManyField(User, related_name='voted_polls', blank=True)
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
